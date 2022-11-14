d20c59a12d8bc7efabf50014f8b67ef8826df210
root <root@ip-172-31-40-170.ap-south-1.compute.internal>
2022-11-14 12:37:50 +0000

    adding server config

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors'
+import cors from 'cors';
+import fs from 'fs'
+import https from 'https'
 import userrouter from './routers/userRuter.js';
 const app = express()
 app.use(express.json());
 app.use(userrouter)
 const PORT = 3000;
+
+var options = {
+    key: fs.readFileSync('../ssl/privatekey.key'),
+    cert: fs.readFileSync('../ssl/certificate.pem')
+};
 
 app.use(cors({ origin: true }));
 
@@ -32,6 +39,19 @@ mongoose.connect("mongodb+srv://preeti:Indore1234@cluster0.qzngd29.mongodb.net/R
 
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`)
-})
+// simple route
+app.get("/", (req, res) => {
+  res.send(
+    `<h1 style='text-align: center'>
+          Wellcome to RSL Backend 
+          <br><br>
+          <b style="font-size: 182px;">😃👻</b>
+      </h1>`
+  );
+});
+
+https.createServer(options, app).listen(3000,() => console.log("App running in port 3000 !"));
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running at ${PORT}`)
+// })
